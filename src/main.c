6dfe5a8892447c8b017d16e990f43353d40cfcee
ovidiu102 <ovidiu1021@gmail.com>
2026-05-22 15:02:16 +0300

    Test

--- src/main.c
+++ src/main.c
@@ -1,21 +1,38 @@
 #include "drivers/gpio/gpio.h"
-#include "drivers/timer/timer0.h"
 #include "bsp/nano.h"
+#include "drivers/interrupt/external_interrupt.h"
+#include "drivers/timer/timer0.h"
+#include <stdint.h>
+#include "drivers/adc/adc.h"
+#include "drivers/pwm/pwm.h"
+
+static void buttonPress(void){
+    GPIO_Toggle(D7);
+}
 
 int main(void) {
-    
     Timer0_Init();
-
-    
-    GPIO_Init(LED_BUILTIN, GPIO_OUTPUT);
+    GPIO_Init(D8, GPIO_INPUT);
+    GPIO_Write(D8, GPIO_HIGH);
+    GPIO_Init(D7, GPIO_OUTPUT);
+    ExtInt_Init(INT_0, EXT_INT_FALLING_EDGE, buttonPress);
+    GPIO_Init(D11, GPIO_OUTPUT)
 
     uint32_t last_time = 0;
+    uint16_t pot_value = 0;
+    uint8_t duty = 0;
 
-    while (1) {
-            
-        if (Millis() - last_time >= 1000) {
+    ADC_Init();
+    PWM_Init(D11, 620);
+
+    while(1){
+        GPIO_Write(D7, GPIO_HIGH);
+        if(Millis() - last_time >= 200){
             last_time = Millis();
-            GPIO_Toggle(LED_BUILTIN);
+            GPIO_Toggle(D11);
         }
+        adc_value = ADC_Read(2);`
+        duty = (uint8_t)(adc_value/4);
+        PWM_SetDutyCycle(D11, duty);
     }
 }
